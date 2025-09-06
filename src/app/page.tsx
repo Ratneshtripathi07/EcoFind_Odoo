7680f41586789776289b43cbf65d21cf41c89c77
RatneshTripathi07 <ratneshgeetatripathi@gmail.com>
2025-09-06 12:53:50 +0530

    initiated next js app build with initial config

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <main>
+            <h1>Welcome to EcoFinds</h1>
+            <p>Your sustainable marketplace for eco-friendly products</p>
+        </main>
+    )
+}
